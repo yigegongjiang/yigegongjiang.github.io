868ec14bdee3782fa82d9f365d02c502cb0db416
yigegongjiang <one.gongjiang@gmail.com>
2023-12-15 16:33:17 +0800

    通过 xcode 快速打开博客文件

---
+++ EditByXcode/EditBlog/EditBlog/EditBlog.h
@@ -0,0 +1,18 @@
+//
+//  EditBlog.h
+//  EditBlog
+//
+//  Created by gebiwanger on 2023/12/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EditBlog.
+FOUNDATION_EXPORT double EditBlogVersionNumber;
+
+//! Project version string for EditBlog.
+FOUNDATION_EXPORT const unsigned char EditBlogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EditBlog/PublicHeader.h>
+
+
